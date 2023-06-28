28ce2340311a5d8f4a4e2e7beb8bff50c1586937
sethukumar0001 <sethukumar0001@gmail.com>
2023-06-28 12:14:43 +0530

    updated table

--- src/components/table.js
+++ src/components/table.js
@@ -15,25 +15,31 @@ const CustomTable = (props) => {
   const [dragOver, setDragOver] = useState("");
   const handleDragStart = (e) => {
     const { id } = e.target;
-    const idx = props.headers.indexOf(id);
-    e.dataTransfer.setData("colIdx", idx);
+    if (id) {
+      const idx = props.headers.indexOf(id);
+      e.dataTransfer.setData("colIdx", idx);
+    }
   };
   const handleDragOver = (e) => e.preventDefault();
   const handleDragEnter = (e) => {
     const { id } = e.target;
-    setDragOver(id);
+    if (id) {
+      setDragOver(id);
+    }
   };
   const handleOnDrop = (e) => {
+    e.preventDefault();
     const { id } = e.target;
-    const droppedColIdx = props.headers.indexOf(id);
-    const draggedColIdx = e.dataTransfer.getData("colIdx");
-    const tempCols = [...props.headers];
-    tempCols[draggedColIdx] = props.headers[droppedColIdx];
-    tempCols[droppedColIdx] = props.headers[draggedColIdx];
-    props.setHeaders(tempCols);
-    setDragOver("");
+    if (id) {
+      const droppedColIdx = props.headers.indexOf(id);
+      const draggedColIdx = e.dataTransfer.getData("colIdx");
+      const tempCols = [...props.headers];
+      tempCols[draggedColIdx] = props.headers[droppedColIdx];
+      tempCols[droppedColIdx] = props.headers[draggedColIdx];
+      props.setHeaders(tempCols);
+      setDragOver("");
+    }
   };
-
   return (
     <div className="container">
       <table className="datatable_table">
@@ -42,16 +48,31 @@ const CustomTable = (props) => {
             {props.headers.map((col) => (
               <th
                 className="datatable_table_th"
-                id={col}
                 key={col}
-                draggable
-                onDragStart={handleDragStart}
                 onDragOver={handleDragOver}
                 onDrop={handleOnDrop}
-                onDragEnter={handleDragEnter}
                 dragOver={col === dragOver}
+                id={col}
               >
-                {formatString(col)}
+                <div
+                  className="datatable_table_th_container"
+                  id={col}
+                  draggable
+                  onDragStart={handleDragStart}
+                  onDragEnter={handleDragEnter}
+                  dragOver={col === dragOver}
+                  style={{
+                    borderLeft: col === dragOver && "2px solid #0091ae",
+                  }}
+                >
+                  <img
+                    src={`/images/drag_indicator.svg`}
+                    className="datatable_table_th_img"
+                  />
+                  <h6 className="datatable_table_th_header_text">
+                    {formatString(col)}
+                  </h6>
+                </div>
               </th>
             ))}
           </tr>
@@ -81,4 +102,3 @@ const CustomTable = (props) => {
 };
 
 export default CustomTable;
-
